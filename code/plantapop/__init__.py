2a3173a84954f3b671ba692a78b08fdc01e62172
pablo-snz <pablo.snz.brm@gmail.com>
2023-10-07 16:37:13 +0200

    feat: creates monolith controller and updates init to inject app on it

--- code/plantapop/__init__.py
+++ code/plantapop/__init__.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
 from plantapop.config import Config
+from plantapop.controller import Controller
 
 app = FastAPI()
 CONFIGMAP = Config.get_instance()
+
+controller = Controller(app)
+controller.register()
 
 __all__ = ["app", "CONFIGMAP"]
---
+++ code/plantapop/controller.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Response
+
+
+async def health_check() -> Response:
+    return Response(status_code=200)
+
+
+class Controller:
+    def __init__(self, app: FastAPI):
+        self.app = app
+
+    def register(self):
+        self.app.add_api_route("/health_check", health_check, methods=["GET"])
+        self._chat()
+        self._discovery()
+        self._geolocation()
+        self._labeling()
+        self._plantory()
+
+    def _chat(self):
+        pass
+
+    def _discovery(self):
+        pass
+
+    def _geolocation(self):
+        pass
+
+    def _labeling(self):
+        pass
+
+    def _plantory(self):
+        pass
