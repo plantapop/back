091f2a7ed9fac6cbca3ed185e8695eacdf88fbcd
pablo-snz <pablo.snz.brm@gmail.com>
2023-11-22 17:00:02 +0100

    fix: fixes container factory

--- code/plantapop/shared/infrastructure/container.py
+++ code/plantapop/shared/infrastructure/container.py
@@ -8,23 +8,16 @@ from plantapop.shared.infrastructure.repository import sqlalchemy_uow
 config = Config.get_instance()
 
 
-engine = create_engine(config.postgres.url, echo=True)
-
-
-class SessionFactory:
-    def __init__(self):
-        self.session = orm.scoped_session(
-            orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
-        )
-
-    def __call__(self):
-        return self.session
-
-    def __del__(self):
-        self.session.close()
+engine = create_engine(config.postgres.url, echo=False)
 
 
 class SessionContainer(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(modules=[endpoints, sqlalchemy_uow])
 
-    session = providers.Factory(SessionFactory)
+    session = providers.Factory(
+        lambda: orm.sessionmaker(
+            bind=engine,
+            autocommit=False,
+            autoflush=False,
+        )()
+    )
