29de818ab8146c3d9680f0054003cdafd64ee20f
pablo-snz <pablo.snz.brm@gmail.com>
2023-11-28 20:22:27 +0100

    feat: replaces TestClient with AsyncClient

--- code/test/acceptance_fixtures.py
+++ code/test/acceptance_fixtures.py
@@ -1,44 +1,38 @@
-import pytest
-import sqlalchemy as sa
-from fastapi.testclient import TestClient
-from sqlalchemy import event
-from sqlalchemy.orm import sessionmaker
+import asyncio
 
-from plantapop import create_app
-from plantapop.config import Config
-from plantapop.shared.infrastructure.repository.database import Base
+import pytest_asyncio
+from httpx import AsyncClient
+from sqlalchemy import event
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from plantapop.shared.infrastructure.controller.app import create_app
+from plantapop.shared.infrastructure.repository.database import engine
+from plantapop.shared.infrastructure.repository.models import init_models
+
+none = asyncio.wait(init_models(engine))
 
 app = create_app()
 
-config = Config.get_instance()
-engine = sa.create_engine(config.postgres.url, echo=True)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# Set up the database once
-Base.metadata.drop_all(bind=engine)
-Base.metadata.create_all(bind=engine)
 
+@pytest_asyncio.fixture
+async def client(event_loop):
+    conn = await engine.connect()
+    trans = await conn.begin()
+    session = AsyncSession(bind=conn)
 
-@pytest.fixture()
-def client():
-    connection = engine.connect()
-    transaction = connection.begin()
-    session = TestingSessionLocal(bind=connection)
+    await conn.begin_nested()
 
-    nested = connection.begin_nested()
+    @event.listens_for(session.sync_session, "after_transaction_end")
+    def restart_savepoint(session, transaction):
+        if transaction.nested and not transaction._parent.nested:
+            session.begin_nested()
 
-    @event.listens_for(session, "after_transaction_end")
-    def end_savepoint(sess, trans):
-        nonlocal nested
-        if not nested.is_active:
-            nested = connection.begin_nested()
-
-    try:
-        with app.session.session.override(session):
-            yield TestClient(app)
-
-    finally:
-        if session.is_active:
-            session.close()
-
-        transaction.rollback()
-        connection.close()
+    with app.session.session.override(session):
+        try:
+            yield AsyncClient(
+                app=app, base_url="http://test"
+            )  # creates new thread / loop for this client and it fails
+        finally:
+            await session.close()
+            await trans.rollback()
+            await conn.close()
