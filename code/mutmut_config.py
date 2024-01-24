abd0c89b01d75d015bc04e9014ef3ab7b8230755
pablo-snz <pablo.snz.brm@gmail.com>
2024-01-24 11:50:06 +0100

    feat: adds mutmut whitelisting

--- code/mutmut_config.py
+++ code/mutmut_config.py
@@ -1,9 +1,16 @@
 USECASES_FOLDERS = [
-    "application",
-    "domain",
+    "application/",
+    "domain/",
 ]
+
+INJECTION_LINES = ["self.uow = ", "self.event_bus = "]
 
 
 def pre_mutation(context):
     if not any(folder in context.filename for folder in USECASES_FOLDERS):
         context.skip = True
+        return
+    context_line = context.current_source_line.strip()
+    if any(line in context_line for line in INJECTION_LINES):
+        context.skip = True
+        return
